7a581b3ee751e856127b40775ae7adb0aecdf82f
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-06-05 02:33:01 +0300

    modified models

--- gram/models.py
+++ gram/models.py
@@ -8,21 +8,45 @@ class Image(models.Model):
     image = CloudinaryField('image')
     name = models.CharField(max_length=60)
     caption = models.CharField(max_length=250)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='app_user')
+    user = models.ForeignKey(User,on_delete = models.CASCADE, related_name='app_user')
     posted_at = models.DateTimeField(auto_now_add=True)
     location = models.CharField(max_length=60, blank=True)
 
+    def __str__(self):
+        return self.name
+    
+    ordering = ['-posted_at']
+    
+    def save_image(self):
+        self.save()
+    
+    def delete_image(self):
+        self.delete()
+     
+
+
 class Profile(models.Model):
     profile_photo = CloudinaryField('image')
-    bio = models.CharField(max_length=255)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=250)
+    user = models.OneToOneField(User,on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.user
+    
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.delete()    
+
+
 
 
 class Like(models.Model):
-    image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name='imagelikes')
-    liker = models.ForeignKey(User, on_delete=models.CASCADE, related_name='userlike')
-
+    image =models.ForeignKey(Image, on_delete = models.CASCADE,related_name='imagelikes')
+    liker=models.ForeignKey(User,on_delete = models.CASCADE,related_name='userlike')    
+    
 class Comment(models.Model):
     comment = models.CharField(max_length=250)
-    image = models.ForeignKey(Image,on_delete = models.CASCADE, related_name='comments')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    image = models.ForeignKey(Image,on_delete = models.CASCADE,related_name='comments')
+    user = models.ForeignKey(User,on_delete = models.CASCADE,related_name='comments')
