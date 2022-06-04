8077e6d391697d2829e088984cd1c2e42eacde84
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-06-04 17:38:22 +0300

    import cloudinary

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudiaryField
+
 
 # Create your models here.
--- instagram/settings.py
+++ instagram/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gram.apps.GramConfig',
-    'bootstrap3'
+    'bootstrap3',
+    'cloudinary'
 ]
 
 MIDDLEWARE = [
@@ -82,6 +87,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+cloudinary.config(
+    cloud_name = 'dgtjsiwow',
+    api_key ='346831399935182',
+    api_secret = 'ofyOVu1Z9VbnIUyx9BJ1oCHWd1w',
+    )
 
 
 # Password validation
