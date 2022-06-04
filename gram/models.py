c257e3c6bce8d06d8ae9dec8f7250430006d9cc7
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-06-04 18:00:31 +0300

    class Image

--- gram/models.py
+++ gram/models.py
@@ -1,5 +1,15 @@
 from django.db import models
-from cloudinary.models import CloudiaryField
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Image(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=60)
+    caption = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='app_user')
+
+
+# class Profile(models.Model):
+#     profile_photo
