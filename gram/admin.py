d20c0f248c4e283e2dec8940c1ec3b82ad29da7c
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-06-05 03:42:33 +0300

    registered admin

--- gram/admin.py
+++ gram/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Image, Like, Comment, Profile
+
+admin.site.register(Image)
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Like)
