d13bdda5151e57812035db09f0eabed0839b4b41
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-06-06 13:05:08 +0300

    fix on the urls

--- gram/urls.py
+++ gram/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 #urlPatterns
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('post/', views.post, name = 'post'),
+    path('logout/',auth_views.LogoutView.as_view(template_name = 'registration/logout.html')),
     path('search/', views.search_results, name = 'search_results'),
     path('user-profile/',views.userprofile, name = 'userprofile'),
     path('update-profile/',views.updateprofile, name = 'updateprofile'),
+    path('like/<image_id>', views.like, name='like'),
+    path('comments/<image_id>', views.comments,name='comments'),
+    path('profile/<pk>',views.profile, name = 'profile'),
+
 ]
