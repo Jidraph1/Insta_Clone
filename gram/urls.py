6c1a6b0c13c81fef73a5f0300ec8583f155c1d11
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-06-06 13:30:51 +0300

    fixing bug at image db

--- gram/urls.py
+++ gram/urls.py
@@ -5,14 +5,15 @@ from django.contrib.auth import views as auth_views
 
 #urlPatterns
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('post/', views.post, name = 'post'),
-    path('logout/',auth_views.LogoutView.as_view(template_name = 'registration/logout.html')),
-    path('search/', views.search_results, name = 'search_results'),
-    path('user-profile/',views.userprofile, name = 'userprofile'),
-    path('update-profile/',views.updateprofile, name = 'updateprofile'),
-    path('like/<image_id>', views.like, name='like'),
-    path('comments/<image_id>', views.comments,name='comments'),
-    path('profile/<pk>',views.profile, name = 'profile'),
+   path('',views.index, name='index'),
+   path('logout/',auth_views.LogoutView.as_view(template_name = 'registration/logout.html')),
+   path('post/',views.post, name = 'post'),
+   path('user-profile/',views.userprofile, name = 'userprofile'),
+   path('update-profile/',views.updateprofile, name = 'updateprofile'),
+   path('search/',views.search_results, name='search_results'),
+   path('like/<image_id>', views.like, name='like'),
+   path('comments/<image_id>', views.comments,name='comments'),
+   path('profile/<pk>',views.profile, name = 'profile'),
+
 
 ]
